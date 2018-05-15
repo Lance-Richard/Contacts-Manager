7a32638a3a94b18fa106814ec3e80db9d2b76dc2
richardcalata <calatar6@gmail.com>
2018-05-15 15:55:16 -0500

    Started if else on main

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -7,14 +7,28 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 import util.Input;
 
 public class ContactsManager {
     public static void main(String[] args) {
         String directory = "data";
         String filename = "contacts.txt";
+        Input userChoice = new Input();
 
-        createFileIfNonExists(directory, filename);
+        System.out.println("1. View contacts.\n" +
+                "2. Add a new contact.\n" +
+                "3. Search a contact by name.\n" +
+                "4. Delete an existing contact.\n" +
+                "5. Exit.\n" +
+                "Enter an option (1, 2, 3, 4 or 5):");
+        int select = userChoice.getInt();
+
+        if(select == 1){
+            System.out.println("Hello");
+        }
+//        createFileIfNonExists(directory, filename);
     }
 
     private static void createFileIfNonExists(String directory, String filename) {
@@ -63,10 +77,14 @@ public class ContactsManager {
         ArrayList<String> list = new ArrayList<>();
         Input input = new Input();
         String name;
-        do{
+        String phoneNumber;
+        do {
             name = input.getString("Please enter a new name and phone number");
             list.add(name);
-        }while (input.yesNo("Do you want to add another name to the list?"));
+            phoneNumber = input.getString("Please enter a phone number for " + name + ".");
+            list.add(phoneNumber);
+
+        }while(input.yesNo("Do you want to add another name and number"));
         return list;
     }
 }
